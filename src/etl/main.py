9fc3eb5a2614208f21da6e155069db34b88be181
Long Tran <long.tran@avanade.com>
2025-09-25 11:39:40 +0200

    add 1 more column

--- src/etl/main.py
+++ src/etl/main.py
@@ -26,15 +26,14 @@ def main():
         raise SystemExit("Missing required env vars: AZURE_STORAGE_ACCOUNT, AZURE_CONTAINER, INPUT_BLOB_PATH, AZURE_SAS_TOKEN")
 
     output_blob = os.getenv("OUTPUT_BLOB_PATH") or infer_output_blob_path(input_blob)
-    add_col_name = os.getenv("ADD_COLUMN_NAME", "ingested_on")
-    add_col_value = os.getenv("ADD_COLUMN_VALUE")  # optional literal
 
     log.info(f"Reading:  wasbs://{container}@{account}.blob.core.windows.net/{input_blob}")
     bsc = make_blob_client(account, sas_token)
     raw = download_blob_to_memory(bsc, container, input_blob)
 
     df = read_csv_bytes(raw)
-    df = add_column(df, add_col_name, add_col_value)
+    df = add_column(df, "Column_01", "New_Value_01")
+    df = add_column(df, "Column_02", "New_Value_02")
 
     out_bytes = write_csv_bytes(df)
     upload_blob_from_memory(bsc, container, output_blob, out_bytes, overwrite=True)
